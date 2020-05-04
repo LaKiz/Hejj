e9f60f409c5c1511e2d5230c37803d523725809d
JulleGy <64408558+JulleGy@users.noreply.github.com>
2020-05-04 19:08:27 +0200

    gg

--- Hejj/Assets/Våra Scripts/GameMana.cs
+++ Hejj/Assets/Våra Scripts/GameMana.cs
@@ -3,10 +3,10 @@ using System.Collections;
 
 public class GameMana : MonoBehaviour {
 
-    //public static GameMana instance = null;
-
     public static GameMana instance = null;
     public BoardManager boardScript;
+    public int playerFoodPoints = 100;						//Starting value for Player food points.
+    [HideInInspector] public bool playersTurn = true;		//Boolean to check if it's players turn, hidden in inspector but public.
 
     private int level = 3;
 
@@ -27,6 +27,13 @@ public class GameMana : MonoBehaviour {
     void InitGame()
     {
         boardScript.SetupScene(level);
+    }
+
+    //GameOver is called when the player reaches 0 food points
+    public void GameOver()
+    {
+        //Disable this GameManager.
+        enabled = false;
     }
 
     // Update is called once per frame
